720eaeb357f1f5d9784dbf234ab437d24630edb5
Tryfle <111710533+Tryflle@users.noreply.github.com>
2023-09-20 13:33:43 -0400

    rough test

--- LinuxLLIwsd.sh
+++ LinuxLLIwsd.sh
@@ -1,1 +1,15 @@
-echo also placeholder
+cd ~/Documents
+mkdir lunar-launcher-inject
+cd ~/Documents/lunar-launcher-inject
+echo please kill the lunar launcher task.
+curl https://github.com/Nilsen84/lunar-launcher-inject/releases/download/v1.3.0/lunar-launcher-inject-linux-1.3.0 -output lli
+curl https://github.com/Weave-MC/Weave-Loader/releases/download/v0.2.4/Weave-Loader-Agent-0.2.4.jar -output Weave-Loader-Agent-0.2.4.jar
+echo 'chmod +x lli' must be run before opening lli.
+cd ~/
+mkdir .weave
+cd ~/.weave
+mkdir mods
+echo put mods in ~/.weave/mods
+cd ~/Documents/lunar-launcher-inject
+chmod +x lli
+./lli
