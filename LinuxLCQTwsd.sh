823029d32abcbd970d86a77d7f4cce51f911c1e1
Tryfle <111710533+Tryflle@users.noreply.github.com>
2023-09-20 13:45:11 -0400

    rough test

--- LinuxLCQTwsd.sh
+++ LinuxLCQTwsd.sh
@@ -1,1 +1,16 @@
-echo placeholder for when i get home
+cd ~/Documents
+mkdir lunar-client-qt
+cd lunar-client-qt
+curl https://github.com/Youded-byte/lunar-client-qt/releases/download/v1.5.4/linux.tar.gz -output youded-lcqt.tar.gz
+curl https://github.com/Weave-MC/Weave-Loader/releases/download/v0.2.4/Weave-Loader-Agent-0.2.4.jar -output Weave-Loader-Agent-0.2.4.jar
+tar -xf youded-lcqt.tar.gz
+cd ~/
+mkdir .weave
+cd ~/.weave
+mkdir mods
+echo put mods in ~/.weave/mods
+echo lcqt should open now, just add weave as an agent. ~/Documents/lunar-client-qt/Weave-Loader-0.2.4.jar
+echo lcqt also has a nice mod manager you can use
+cd ~/Documents/lunar-client-qt/Lunar Client QT/
+./lunar-client-qt
+
