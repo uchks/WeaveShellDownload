a167057b13b7092c1878c60743ab3d3d6a2bb428
Tryfle <111710533+Tryflle@users.noreply.github.com>
2023-09-20 16:56:47 -0400

    rewrite

--- MacLLIwsd.sh
+++ MacLLIwsd.sh
@@ -1,14 +1,21 @@
-cd ~/Documents
-mkdir lunar-launcher-inject
+#!/bin/bash
+
+echo "Attempting to end task: lunarclient"
+killall -KILL lunarclient
+
+mkdir -p ~/Documents/lunar-launcher-inject
 cd ~/Documents/lunar-launcher-inject
-curl https://github.com/Nilsen84/lunar-launcher-inject/releases/download/v1.3.0/lunar-launcher-inject-mac-1.3.0 -output lli
-curl https://github.com/Weave-MC/Weave-Loader/releases/download/v0.2.4/Weave-Loader-Agent-0.2.4.jar -output Weave-Loader-Agent-0.2.4.jar
-cd ~/
-mkdir .weave
-cd ~/.weave
-mkdir mods
-cd ~/Documents/lunar-launcher-inject
+
+echo "Before you open LLI:"
+echo "Lunar's launcher must not be running at all, not even in the background."
+echo "You must run 'chmod +x lli' whilst in the directory."
+
+curl -L -o lli "https://github.com/Nilsen84/lunar-launcher-inject/releases/download/v1.3.0/lunar-launcher-inject-mac-1.3.0"
+curl -L -o Weave-Loader-Agent.0.2.4.jar "https://github.com/Weave-MC/Weave-Loader/releases/download/v0.2.4/Weave-Loader-Agent-0.2.4.jar"
+
+mkdir -p ~/.weave/mods
+echo "Put your modifications in ~/.weave/mods"
+
+echo "Lunar will open soon, just launch and weave will be attached."
 chmod +x lli
-echo you will have to run 'chmod +x lli' and run the launcher through lli every time you want to use weave. since you are on Mac after chmod it should be okay to double click it.
-echo the script will attempt to open lli, but if lunar client's launcher is open it won't work and will need to be done manually
-./lli
+timeout 5s ./lli
